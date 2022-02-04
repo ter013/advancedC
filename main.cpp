623b4e152e842f4834f335184360e4710e3677d4
ter013 <kadykov.ao@phystech.edu>
2022-02-04 10:27:42 +0300

    первый цикл

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace  std;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    for(int i = 0; i < 10; i++) {
+        cout << i * i;
+    }
+    return 0;
+}
